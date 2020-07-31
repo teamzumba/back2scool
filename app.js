1cabfa76b4e24183646093991a5b003c6536fa99
jaguar017 <jayesh.singh2019@vitstudent.ac.in>
2020-08-01 03:01:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+
+const userSchema ={
+  email :String,
+  password :String
+}
+
+
+const User = new mongoose.model("User",userSchema);
 
 //landing Page 
 app.get("/",function(req,res)
@@ -17,47 +30,48 @@ app.get("/",function(req,res)
 
 //routes
 
-app.post("/index",function(req,res)
-{
-  res.render("login_spellbee")
-  app.post("/login_spellbee",function(req,res){
-    res.render("spellbee")
-  });
-});
-
-
 app.post("/index",function(req,res){
   res.render("home")
-  app.post("/home",function(req,res){
-    res.render("mathbee")
-  });
 });
 
-
-
-app.get("/spellbee",function(req,res)
-{
-  res.render("login_spellbee")
-  app.post("/login_spellbee",function(req,res){
-    res.render("spellbee")
-  });
-});
-
-
-app.get("/mathbee",function(req,res)
-{
+app.get("/login",function(req,res){
   res.render("home")
-  app.post("/home",function(req,res){
-    res.render("mathbee")
-  });
-
 });
 
+app.post("/login",function(req,res){
+  var subject=req.body.test;
+  const username = req.body.username;
+ const password = req.body.password;
+ console.log(username);
+ console.log(subject);
+ console.log(password);
+ User.findOne({email :username},function(err, foundUser){
+  if (err) {
+     console.log(err);
+  } else{
+     if(foundUser.password === password && subject === "mathbee"){
+             res.render("mathbee");
+        console.log(foundUser.password);
 
-
+      }else if(foundUser.password === password && subject === "spellbee")
+      {
+        res.render("spellbee")
+      }else{
+        console.log("error")
+      }
+   }
+   });
+   
+ });
+app.get("/mathbee",function(req,res){
+  res.render("home");
+});
+app.get("/spellbee",function(req,res){
+  res.render("home");
+});
 
 
 app.listen(3000,function()
 {
     console.log("listening on 3000");
-})
+});
